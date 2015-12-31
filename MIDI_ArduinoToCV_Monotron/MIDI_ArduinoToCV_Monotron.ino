e41369107c27e0f240867e8b10e666181c0f3c5c
inakidml <inaki.diaz@ikasle.egibide.org>
2015-12-31 14:46:18 +0100

    Frecs de una escala para ir probando

--- MIDI_ArduinoToCV_Monotron/MIDI_ArduinoToCV_Monotron.ino
+++ MIDI_ArduinoToCV_Monotron/MIDI_ArduinoToCV_Monotron.ino
@@ -1,1 +1,29 @@
+float a4=440;
+float Bb4=466.16;
+float B4 = 493.88;
+float C5 = 523.25;
+float Cs5 = 554.37;
+float D5 = 587.33;
+float Eb5 = 622.23;
+float E5 = 659.26;
+float F5 = 698.46;
+float Fs5 = 739.99;
+float G5 = 783.99;
+float Ab5 = 830.61;
+float a5 = 880.00;
+int pitchPin=11;
+void setup(){
+Serial.begin(9600);
+delay(200);
+Serial.println("serial arrancado");
+pinMode(pitchPin, OUTPUT);
 
+}
+void loop(
+
+
+
+){
+
+
+}

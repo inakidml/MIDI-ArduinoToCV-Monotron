da339a70094783a3e644460d4cca261edb4437a3
diazibon <diazibon@gmail.com>
2016-01-23 17:31:35 +0100

    Añadido midi in

--- MIDI_ArduinoToCV_Monotron/MIDI_ArduinoToCV_Monotron.ino
+++ MIDI_ArduinoToCV_Monotron/MIDI_ArduinoToCV_Monotron.ino
@@ -1,40 +1,38 @@
+#include <SoftwareSerial.h>
+
 #include <SPI.h>
 
 #include <MCPDAC.h>
 
 
-
-float a4=440;
-float Bb4=466.16;
-float B4 = 493.88;
-float C5 = 523.25;
-float Cs5 = 554.37;
-float D5 = 587.33;
-float Eb5 = 622.23;
-float E5 = 659.26;
-float F5 = 698.46;
-float Fs5 = 739.99;
-float G5 = 783.99;
-float Ab5 = 830.61;
-float a5 = 880.00;
- int i=0;
-
+SoftwareSerial portOne(8, 9);
+byte commandByte;
+byte noteByte;
+byte velocityByte;
 //int pitchPin=DAC0;
+
 int gatePin=5;
 int x;
 int nota;
-int volts =0;
+int volts=0;
 
 
 void setup(){
-  Serial.begin(9600);
+ Serial.begin(9600);
+ pinMode(13,OUTPUT);
+  digitalWrite(13,HIGH);
+  portOne.begin(31250);
+
+ 
   delay(200);
-  Serial.println("serial arrancado");
+//  Serial.println("serial arrancado");
 //  pinMode(pitchPin, OUTPUT);
   pinMode(gatePin, OUTPUT);
  // analogWriteResolution(12);
  // CS on pin 10, no LDAC pin (tie it to ground).
-  MCPDAC.begin(10);
+  
+  
+  //MCPDAC.begin(10);
   
   // Set the gain to "HIGH" mode - 0 to 4096mV.
   MCPDAC.setGain(CHANNEL_A,GAIN_HIGH);
@@ -45,22 +43,76 @@ void setup(){
  
 }
 
+void checkMIDI(){
+ /* do{
+    if (portOne.available()){
+      commandByte = portOne.read();//read first byte
+      noteByte = portOne.read();//read next byte
+      velocityByte = portOne.read();//read final byte
+      Serial.print((commandByte),HEX);
+      Serial.print(" ");
+      Serial.print((noteByte),HEX );
+      Serial.print(" ");
+      Serial.println((velocityByte), HEX);
+    }
+  }
+  while (portOne.available() > 2);//when at least three bytes available
+  */
+   while(portOne.available()){
+    if (portOne.available()){
+      commandByte = portOne.read();//read first byte
+      
+      Serial.println((commandByte),HEX);
+     delay(5) ;
+      
+    }
+  }
+
+}
+
+/*void MIDImessage(int MIDIcommand, int MIDInote, int MIDIvelocity) {
+  portOne.write(MIDIcommand);//send note on or note off command 
+  portOne.write(MIDInote);//send pitch data
+  portOne.write(MIDIvelocity);//send velocity data
+}
+*/
 void loop(){
  
   
   //Serial.println(map(analogRead(A0),0,1024,0,5000));
-  //delay(100 );
- // volts=2048;
-  digitalWrite(gatePin, HIGH);
+
+  
+  checkMIDI();
+  //MIDImessage(commandByte, noteByte, velocityByte);
+  //portOne.write(commandByte);
+  if (commandByte==144){
+    
+      digitalWrite(gatePin, HIGH);
+      digitalWrite(13,HIGH);
+     //MCPDAC.setVoltage(CHANNEL_A,map(noteByte,36,84,0,4096));
+      //delay(10);
+  }
+  else {
+      if (commandByte==144&&velocityByte==0 || commandByte==128){
+        digitalWrite(gatePin, LOW);
+        digitalWrite(13,LOW);
+      }
+  }
+  //delay(200);
+  digitalWrite(gatePin,LOW);
+  digitalWrite(13,LOW);
+  
+/*  digitalWrite(gatePin, HIGH);
   MCPDAC.setVoltage(CHANNEL_A,volts&0x0fff);
   Serial.println(map(analogRead(A0),0,1024,0,5000));
   //Serial.print("volts= ");
-  //Serial.println(volts);
+  Serial.println(volts&0x0fff);
   //volts+=100;
   //i++;
   
   delay(2000);
    volts=volts+1024/12;
+*/   
 //  digitalWrite(gatePin, HIGH);
 //  MCPDAC.setVoltage(CHANNEL_A,volts&0x0fff);
   //Serial.println(map(analogRead(A0),0,1024,0,5000));
